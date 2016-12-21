65f020672bf4e3c5c3b4fa7ff18b9b0c9604bd74
Jayvee-Air <jayveehe@gmail.com>
2016-12-22 00:46:46 +0800

    丰富api

--- utils/cloudmusic_api.py
+++ utils/cloudmusic_api.py
@@ -80,6 +80,28 @@ def user_playlist(uid, offset=0, limit=100):
         return -1
 
 
+def song_detail(song_ids, offset=0):
+    """
+    根据歌曲id列表批量获取歌曲详情
+    :param song_ids:
+    :return:
+    """
+    tmpids = song_ids[offset:]
+    tmpids = tmpids[0:100]
+    tmpids = list(map(str, tmpids))
+    base_url = 'http://music.163.com/api/song/detail?ids=[%s]' % (  # NOQA
+        ','.join(tmpids))
+    try:
+        data = json.loads(requests.get(base_url).content)
+        # the order of data['songs'] is no longer the same as tmpids,
+        # so just make the order back
+        data['songs'].sort(key=lambda song: tmpids.index(str(song['id'])))
+        return data['songs']
+    except requests.exceptions.RequestException as e:
+        data_process_logger.error(e)
+        return []
+
+
 def search_web(s_name, type, limit=10):
     """
     网页搜索api
@@ -100,10 +122,31 @@ def search_web(s_name, type, limit=10):
         return -1
 
 
+def user_details(uid):
+    """
+    根据uid获取用户详情(通过歌单列表间接获取creator信息)
+    :param uid:
+    :return:
+    """
+    # upl = user_playlist(uid, limit=1)
+    base_url = 'http://music.163.com/api/user/playlist/?offset=%s&limit=%s&uid=%s' % (0, 0, uid)
+    # data = {'offset': offset, 'limit': limit, 'uid': uid}
+    try:
+        # data = urlencode(data)
+        res = requests.get(base_url, headers=header).content
+        data = json.loads(res)
+        return data['playlist'][0]['creator']
+    except (requests.exceptions.RequestException, KeyError) as e:
+        data_process_logger.error(e)
+        return -1
+
+
 if __name__ == '__main__':
     pass
     # a = playlist_detail('3659853')
     # print a
     # b = user_playlist('2886507')
     # print b
-    # print search_web('jayvee he', '1002', 10)
+    # c = song_detail(['37239018', '23'])
+    d = user_details('2886507')
+    print search_web('jayvee he', '1002', 10)
