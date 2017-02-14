bc4cf62a61e5dee78b8229e4bb379cfdcf1f072f
Jayvee-Air <jayveehe@gmail.com>
2017-02-14 11:19:53 +0800

    1. 为各爬取API增加重试机制; 2. 增加用户关注和粉丝信息的爬取函数

--- utils/cloudmusic_api.py
+++ utils/cloudmusic_api.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import hashlib
 import json
+import random
 from urllib import urlencode
 
 import requests
@@ -30,6 +31,11 @@ header = {
 }
 cookies = {'appver': '1.5.2'}
 
+# proxylist = [{}, {'http': '138.197.118.48:80'}, {'http': '104.196.224.28:80'}]
+proxylist = [None]
+
+retry_times = 3
+
 
 def user_login(username, password):
     """
@@ -53,7 +59,7 @@ def user_login(username, password):
     # s = requests.session()
     # s.headers = header
     try:
-        res = requests.post(base_url, data=data, headers=header).content
+        res = requests.post(base_url, data=data, headers=header, proxies=random.choice(proxylist)).content
         result = json.loads(res)
         return result
     except Exception, e:
@@ -61,21 +67,30 @@ def user_login(username, password):
         return -1
 
 
-def playlist_detail(playlist_id):
+def playlist_detail(playlist_id, limit=2000):
     """
     根据歌单id获取歌单的详情
     :param playlist_id:playlist id
     :return: playlist info json text
+
+    Args:
+        limit:
     """
-    base_url = 'http://music.163.com/api/playlist/detail?id=%s' % playlist_id
-    res = requests.get(base_url, headers=header).content
-    # print res
-    return json.loads(res)
+    for i in range(retry_times):
+        try:
+            base_url = 'http://music.163.com/api/playlist/detail?id=%s&limit=%s' % (playlist_id, limit)
+            res = requests.get(base_url, headers=header).content
+            # print res
+            return json.loads(res)
+        except Exception, e:
+            data_process_logger.error('%s playlist failed, reason = %s' % (playlist_id, e))
+            continue
+    return -1
 
 
-def user_playlist(uid, offset=0, limit=100):
+def user_playlist(uid, offset=0, limit=1000):
     """
-    根据用户id获取用户歌单
+    根据用户id获取用户歌单编号
     :param uid:
     :param offset:
     :param limit:
@@ -83,14 +98,15 @@ def user_playlist(uid, offset=0, limit=100):
     """
     base_url = 'http://music.163.com/api/user/playlist/?offset=%s&limit=%s&uid=%s' % (offset, limit, uid)
     # data = {'offset': offset, 'limit': limit, 'uid': uid}
-    try:
-        # data = urlencode(data)
-        res = requests.get(base_url, headers=header).content
-        data = json.loads(res)
-        return data['playlist']
-    except (requests.exceptions.RequestException, KeyError) as e:
-        data_process_logger.error(e)
-        return -1
+    for i in range(retry_times):
+        try:
+            # data = urlencode(data)
+            res = requests.get(base_url, headers=header).content
+            data = json.loads(res)
+            return data['playlist']
+        except (requests.exceptions.RequestException, KeyError) as e:
+            data_process_logger.error(e)
+    return -1
 
 
 def song_detail(song_ids, offset=0):
@@ -104,18 +120,19 @@ def song_detail(song_ids, offset=0):
     tmpids = list(map(str, tmpids))
     base_url = 'http://music.163.com/api/song/detail?ids=[%s]' % (  # NOQA
         ','.join(tmpids))
-    try:
-        data = json.loads(requests.get(base_url).content)
-        # the order of data['songs'] is no longer the same as tmpids,
-        # so just make the order back
-        data['songs'].sort(key=lambda song: tmpids.index(str(song['id'])))
-        return data['songs']
-    except requests.exceptions.RequestException as e:
-        data_process_logger.error(e)
-        return []
+    for i in range(retry_times):
+        try:
+            data = json.loads(requests.get(base_url).content)
+            # the order of data['songs'] is no longer the same as tmpids,
+            # so just make the order back
+            data['songs'].sort(key=lambda song: tmpids.index(str(song['id'])))
+            return data['songs']
+        except requests.exceptions.RequestException as e:
+            data_process_logger.error(e)
+    return []
 
 
-def user_details(uid):
+def user_profile(uid):
     """
     根据uid获取用户详情(通过歌单列表间接获取creator信息)
     :param uid:
@@ -124,14 +141,70 @@ def user_details(uid):
     # upl = user_playlist(uid, limit=1)
     base_url = 'http://music.163.com/api/user/playlist/?offset=%s&limit=%s&uid=%s' % (0, 0, uid)
     # data = {'offset': offset, 'limit': limit, 'uid': uid}
-    try:
-        # data = urlencode(data)
-        res = requests.get(base_url, headers=header).content
-        data = json.loads(res)
-        return data['playlist'][0]['creator']
-    except (requests.exceptions.RequestException, KeyError) as e:
-        data_process_logger.error(e)
-        return -1
+    for i in range(retry_times):
+        try:
+            # data = urlencode(data)
+            res = requests.get(base_url, headers=header, proxies=random.choice(proxylist)).content
+            data = json.loads(res)
+            return data['playlist'][0]['creator']
+        except (requests.exceptions.RequestException, KeyError) as e:
+            data_process_logger.error(e)
+            data_process_logger.warn('retry')
+            continue
+    return -1
+
+
+def user_follows(uid):
+    """
+    根据uid获取用户关注列表
+    :param uid:
+    :return:
+    """
+    # upl = user_playlist(uid, limit=1)
+    base_url = 'http://music.163.com/weapi/user/getfollows/%s?csrf_token=5c49a1f533701460855c496b9be5579f' % (uid)
+    # data = {'offset': offset, 'limit': limit, 'uid': uid}
+    for i in range(retry_times):
+        try:
+            # data = urlencode(data)
+            text = {
+                'limit': 1000, 'offset': 0
+            }
+            data = encrypted_request(text)
+            res = requests.post(base_url, data=data, headers=header, proxies=random.choice(proxylist)).content
+            data = json.loads(res)
+            return data['follow']
+        except (requests.exceptions.RequestException, KeyError) as e:
+            data_process_logger.error(e)
+            data_process_logger.warn('retry')
+            continue
+    return -1
+
+
+def user_fans(uid):
+    """
+    根据uid获取用户粉丝列表
+    :param uid:
+    :return:
+    """
+    # upl = user_playlist(uid, limit=1)
+    base_url = 'http://music.163.com/weapi/user/getfolloweds/?csrf_token=5c49a1f533701460855c496b9be5579f'
+    # data = {'offset': offset, 'limit': limit, 'uid': uid}
+    for i in range(retry_times):
+        try:
+            # data = urlencode(data)
+            text = {
+                'userId': uid,
+                'limit': 1000, 'offset': 0
+            }
+            data = encrypted_request(text)
+            res = requests.post(base_url, data=data, headers=header, proxies=random.choice(proxylist)).content
+            data = json.loads(res)
+            return data['followeds']
+        except (requests.exceptions.RequestException, KeyError) as e:
+            data_process_logger.error(e)
+            data_process_logger.warn('retry')
+            continue
+    return -1
 
 
 def search_web(s_name, type, limit=10):
@@ -144,22 +217,26 @@ def search_web(s_name, type, limit=10):
     """
     data = {"s": s_name, "type": type, "limit": limit}
     search_url = 'http://music.163.com/api/search/get/web'
-    try:
-        d = urlencode(data)
-        res = requests.post(search_url, d, headers=header).content
-        data = json.loads(res)
-        return data
-    except (requests.exceptions.RequestException, KeyError) as e:
-        data_process_logger.error(e)
-        return -1
+    for i in range(retry_times):
+        try:
+            d = urlencode(data)
+            res = requests.post(search_url, d, headers=header, proxies=random.choice(proxylist)).content
+            data = json.loads(res)
+            return data
+        except (requests.exceptions.RequestException, KeyError) as e:
+            data_process_logger.error(e)
+    return -1
 
 
 if __name__ == '__main__':
     pass
-    # a = playlist_detail('3659853')
+    a = playlist_detail(85224880,limit=500)
     # print a
-    # b = user_playlist('2886507')
+    b = user_playlist('2886507', limit=1000)
     # print b
-    # c = song_detail(['37239018', '23'])
-    d = user_details('2886507')
+    c = song_detail(['37239018', '23'])
+    # d = user_profile('2886507')
+    # f = user_follows('2886507')
+    # fans = user_fans('2886507')
+    # i = user_infos('2886507')
     print search_web('jayvee he', '1002', 10)
