7931e9c13e57a5c47c5c225fd9517e500eb47d69
Jayvee-Air <jayveehe@gmail.com>
2017-02-14 11:17:27 +0800

    增加避免重复存储的数据库操作

--- utils/cloudmusic_dao.py
+++ utils/cloudmusic_dao.py
@@ -9,18 +9,34 @@ from utils.db_utils import get_db_inst
 from utils.logger_utils import data_process_logger
 
 
-class CloudMusicDAO():
-    def __init__(self, db_name='MusicTaster'):
+class CloudMusicDAO:
+    def __init__(self, db_name, collection_name):
         self.db_name = db_name
+        self.collection_name = collection_name
+        self.db_inst = get_db_inst(self.db_name, self.collection_name)
 
-    def get_user_info_dbinst(self):
+    def save_unique_item(self, data_obj, primary_key='userId', is_overwrite=False, is_inform=False):
         """
-        获取用户信息数据的数据库实例
+        存储数据对象,并避免重复存储
+        Args:
+            data_obj:
+            primary_key:
+            is_overwrite:
+
         Returns:
-            数据实例
+
         """
-        try:
-            user_dbinst = get_db_inst(self.db_name, 'UserInfos')
-            return user_dbinst
-        except Exception, e:
-            data_process_logger.error('get_user_info_dbinst failed, details=%s' % e)
+        find_result = self.db_inst.find({primary_key: data_obj[primary_key]})
+        # is_exist = user_dbinst.find({'userId': userinfo['userId']}).count() != 0
+        # print find_result.count()
+        if not find_result.count():
+            self.db_inst.insert(data_obj)
+        elif is_overwrite:
+            self.db_inst.update({primary_key: data_obj[primary_key]}, data_obj)
+            if is_inform:
+                data_process_logger.warn(
+                    'overwrite item %s in %s' % (data_obj[primary_key], self.collection_name))
+        else:
+            if is_inform:
+                data_process_logger.warn(
+                    'Item %s exist! in %s' % (data_obj[primary_key], self.collection_name))
